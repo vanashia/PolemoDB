4cbac852adbc97da56a1756474381a712d5fea39
vanashia <1308590146@qq.com>
2026-09-21 21:51:31 +0800

    cmake: respect disabled gpfdist in target checks

--- cmake/tests/validate_native_targets.cmake
+++ cmake/tests/validate_native_targets.cmake
@@ -6,12 +6,19 @@ endif()
 
 file(READ "${GPDB_BINARY_DIR}/build.ninja" _build_ninja)
 
-foreach(_target IN ITEMS
-    gpdb-generated gpdb-server gpdb-clients gpdb postgres pg_ctl psql gpfdist
+set(_gpdb_expected_targets
+    gpdb-generated gpdb-server gpdb-clients gpdb postgres pg_ctl psql
     libpq_shared pg_regress regress autoinc refint test_hook
     query_info_hook_test gp_toolkit gp_ao_co_diagnostics gp_workfile_mgr
     gp_session_state_memory_stats gp_instrument_shmem pageinspect gp_inject_fault
     gp_debug_numsegments gpdb-regression-tools gpdb-regression)
+file(STRINGS "${GPDB_BINARY_DIR}/CMakeCache.txt" _gpdb_gpfdist_enabled
+     REGEX "^GPDB_ENABLE_GPFDIST:BOOL=ON$")
+if(_gpdb_gpfdist_enabled)
+  list(APPEND _gpdb_expected_targets gpfdist)
+endif()
+
+foreach(_target IN LISTS _gpdb_expected_targets)
   string(FIND "${_build_ninja}" "build ${_target}:" _target_pos)
   if(_target_pos EQUAL -1 AND NOT EXISTS "${GPDB_BINARY_DIR}/CMakeFiles/${_target}.dir")
     message(FATAL_ERROR "native target was not generated: ${_target}")
